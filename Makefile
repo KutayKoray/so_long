73442f17776c200f7ad3062d2e23befe523b9349
Kutay Koray <kkoray@k1m08s07.42kocaeli.com.tr>
2025-01-18 02:28:49 +0300

    refactor: Makefile and header.

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ GNL_DIR = get_next_line
 MLX_DIR = minilibx-linux
 TEX_DIR = textures
 MAP_DIR = maps
+PRINTF_DIR = ft_printf
 
 SRC_FILES = $(SRC_DIR)/main.c \
             $(GNL_DIR)/get_next_line.c \
@@ -14,27 +15,37 @@ SRC_FILES = $(SRC_DIR)/main.c \
 			$(SRC_DIR)/mlx_image_utils.c \
 			$(SRC_DIR)/free.c \
 			$(SRC_DIR)/check_map.c \
+			$(SRC_DIR)/valid_path.c \
+			$(SRC_DIR)/exit.c \
+			$(SRC_DIR)/check_walls.c \
+			$(SRC_DIR)/keyhook_utils.c \
 
 OBJ_FILES = $(SRC_FILES:.c=.o)
 
 NAME = so_long
 
 MLX_LIB = $(MLX_DIR)/libmlx.a
+PRINTF_LIB = $(PRINTF_DIR)/libftprintf.a
 
 all: $(NAME)
 
-$(NAME): $(OBJ_FILES) $(MLX_LIB)
-	$(CC) $(CFLAGS) $(OBJ_FILES) -L$(MLX_DIR) -lmlx -lX11 -lXext -o $(NAME)
+$(NAME): $(OBJ_FILES) $(MLX_LIB) $(PRINTF_LIB)
+	$(CC) $(CFLAGS) $(OBJ_FILES) -L$(MLX_DIR) -lmlx -L$(PRINTF_DIR) -lftprintf -lX11 -lXext -o $(NAME)
 
 $(MLX_LIB):
 	@make -C $(MLX_DIR)
 
+$(PRINTF_LIB):
+	@make -C $(PRINTF_DIR)
+
 clean:
 	rm -f $(OBJ_FILES)
 	@make clean -C $(MLX_DIR)
+	@make clean -C $(PRINTF_DIR)
 
 fclean: clean
 	rm -f $(NAME)
+	@make fclean -C $(PRINTF_DIR)
 
 re: fclean all
 
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: kkoray <kkoray@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:07:50 by kkoray            #+#    #+#             */
-/*   Updated: 2025/01/17 22:52:00 by kkoray           ###   ########.fr       */
+/*   Updated: 2025/01/18 02:25:36 by kkoray           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,80 @@
 
 typedef struct s_coordinates
 {
-	int		x;
-	int		y;
-}			t_coordinates;
+	int				x;
+	int				y;
+}					t_coordinates;
+
+typedef struct s_char_counts
+{
+	int				collectibles;
+	int				exits;
+	int				players;
+}					t_char_counts;
+
+typedef struct s_map
+{
+	char			**map;
+	int				map_width;
+	int				map_height;
+	t_coordinates	start_coord;
+	int				collected_all_c;
+	int				reached_e;
+}					t_map;
 
 typedef struct s_data
 {
-	int		collectable_count;
-	int		is_all_collected;
-	int		map_width;
-	int		map_height;
-	int		img_width;
-	int		img_height;
-	char	**map;
-	void	*mlx;
-	void	*window;
-	void	*wall;
-	void	*road;
-	void	*collect;
-	void	*player;
-	void	*exit;
-	int		player_x;
-	int		player_y;
-	int		number_of_moves;
-}			t_data;
+	int				collectable_count;
+	int				is_all_collected;
+	int				map_width;
+	int				map_height;
+	int				img_width;
+	int				img_height;
+	char			**map;
+	void			*mlx;
+	void			*window;
+	void			*wall;
+	void			*road;
+	void			*collect;
+	void			*player;
+	void			*exit;
+	int				player_x;
+	int				player_y;
+	int				number_of_moves;
+}					t_data;
 
-void		key_hook_d(t_data *map_data);
-void		key_hook_a(t_data *map_data);
-void		key_hook_w(t_data *map_data);
-void		key_hook_s(t_data *map_data);
-void		key_hook_e(t_data *map_data);
-int			key_hook(int keycode, void *param);
+void				key_hook_d(t_data *map_data);
+void				key_hook_a(t_data *map_data);
+void				key_hook_w(t_data *map_data);
+void				key_hook_s(t_data *map_data);
+void				key_hook_e(t_data *map_data);
+int					key_hook(int keycode, void *param);
 
-void		check_exit(t_data *map_data);
-void		check_collected(t_data *map_data);
-int			can_exit(t_data *map_data, char dir);
-int			check_wall(t_data *map_data, char dir);
+void				check_exit(t_data *map_data);
+void				check_collected(t_data *map_data);
+int					can_exit(t_data *map_data, char dir);
+int					check_wall(t_data *map_data, char dir);
 
-void		mlx_image_wall(t_data *map_data);
-void		mlx_image_road(t_data *map_data);
-void		mlx_image_collect(t_data *map_data);
-void		mlx_image_player(t_data *map_data);
-void		mlx_image_door(t_data *map_data);
+void				mlx_image_wall(t_data *map_data);
+void				mlx_image_road(t_data *map_data);
+void				mlx_image_collect(t_data *map_data);
+void				mlx_image_player(t_data *map_data);
+void				mlx_image_door(t_data *map_data);
 
-void		free_data(t_data *map_data);
+void				free_data(t_data *map_data);
+void				free_map(char **map);
+void				free_map_info(t_map *map_info);
+void				error_exit(t_data *map_data);
+int					correct_exit(t_data *map_data);
+void				print_error_exit(char *error_message, t_map *map_info);
 
-void		check_dir_exist(char **argv);
-void		validate_map(char **map);
-char		**read_map(char **argv);
+void				check_dir_exist(char **argv);
+void				validate_map(char **map);
+char				**read_map(char **argv);
+int					check_valid_path(t_map *map_info);
+
+void				destroy_mlx_images(t_data *map_data);
+void				check_surrounded_by_walls(t_map *map_info);
+int					get_map_height(char **argv);
 
 #endif
